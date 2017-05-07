b3774999c89b2e165ca2799580cca4c366925884
Tomasz Zieliński <tztmp42@gmail.com>
2017-05-07 23:12:55 +0200

    HID-10 Fix wrong flash use warnings, use session instead

--- app/helpers/SessionMessages.java
+++ app/helpers/SessionMessages.java
@@ -4,28 +4,28 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import play.libs.Json;
 
-import static play.mvc.Controller.flash;
+import static play.mvc.Controller.session;
 
 public class SessionMessages {
     static public void addError(String m) {
-        String messages = flash().get("_messages_error");
+        String messages = session().get("_messages_error");
         ArrayNode json = messages == null ? JsonNodeFactory.instance.arrayNode() : (ArrayNode) Json.parse(messages);
         json.add(m);
-        flash("_messages_error", json.toString());
+        session("_messages_error", json.toString());
     }
 
     static public void addWarning(String m) {
-        String messages = flash().get("_messages_warning");
+        String messages = session().get("_messages_warning");
         ArrayNode json = messages == null ? JsonNodeFactory.instance.arrayNode() : (ArrayNode) Json.parse(messages);
         json.add(m);
-        flash("_messages_warning", json.toString());
+        session("_messages_warning", json.toString());
     }
 
     static public void addSuccess(String m) {
-        String messages = flash().get("_messages_success");
+        String messages = session().get("_messages_success");
         ArrayNode json = messages == null ? JsonNodeFactory.instance.arrayNode() : (ArrayNode) Json.parse(messages);
         json.add(m);
-        flash("_messages_success", json.toString());
+        session("_messages_success", json.toString());
     }
 
 }
