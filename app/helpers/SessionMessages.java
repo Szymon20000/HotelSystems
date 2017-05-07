2f44f9069f6147b9386a41109ffa12d143872363
rafalk342 <rafalk342@gmail.com>
2017-05-07 19:01:25 +0200

    HID-12 Added SessionMessages categories

--- app/helpers/SessionMessages.java
+++ app/helpers/SessionMessages.java
@@ -7,10 +7,25 @@ import play.libs.Json;
 import static play.mvc.Controller.flash;
 
 public class SessionMessages {
-    static public void addMessage(String m) {
-        String messages = flash().get("_messages");
+    static public void addError(String m) {
+        String messages = flash().get("_messages_error");
         ArrayNode json = messages == null ? JsonNodeFactory.instance.arrayNode() : (ArrayNode) Json.parse(messages);
         json.add(m);
-        flash("_messages", json.toString());
+        flash("_messages_error", json.toString());
     }
+
+    static public void addWarning(String m) {
+        String messages = flash().get("_messages_warning");
+        ArrayNode json = messages == null ? JsonNodeFactory.instance.arrayNode() : (ArrayNode) Json.parse(messages);
+        json.add(m);
+        flash("_messages_warning", json.toString());
+    }
+
+    static public void addSuccess(String m) {
+        String messages = flash().get("_messages_success");
+        ArrayNode json = messages == null ? JsonNodeFactory.instance.arrayNode() : (ArrayNode) Json.parse(messages);
+        json.add(m);
+        flash("_messages_success", json.toString());
+    }
+
 }
