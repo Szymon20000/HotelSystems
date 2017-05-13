0bd6406237fd44dbbf29fd7c49108e08f847f486
monsterkaktus <piotrekmiko.24@onet.eu>
2017-05-13 17:03:47 +0200

    View of the rooms (controller + view, change in routes) - corrected, few photos, css

--- app/controllers/ShowRooms.java
+++ app/controllers/ShowRooms.java
@@ -1,13 +1,15 @@
 package controllers;
 
 import models.Model;
-import models.Rooms;
+import models.Photo;
+import models.Room;
 import play.data.FormFactory;
 import play.db.Database;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 import javax.inject.Inject;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 public class ShowRooms extends Controller {
@@ -28,16 +30,43 @@ public class ShowRooms extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() throws NoSuchFieldException, IllegalAccessException, InstantiationException {
-        List<Rooms> ListOfRooms = Model.<Rooms>findAll(Rooms.class);
-        Rooms room1 = new Rooms();
-        Rooms room2 = new Rooms();
-        room1.setId(1);
-        room2.setId(2);
-        room1.setFloor(3);
+        List<Room> ListOfRoom = Model.<Room>findAll(Room.class);
+        Room room1 = new Room();
+        Room room2 = new Room();
+        Room room3 = new Room();
+        room1.setPrice(BigDecimal.valueOf(1000));
+        room2.setPrice(BigDecimal.valueOf(1000));
+        room3.setPrice(BigDecimal.valueOf(1500));
+        room1.setId(10);
+        room2.setId(20);
+        room3.setId(30);
+        room1.setFloor(1);
         room2.setFloor(2);
-        ListOfRooms.add(room1);
-        ListOfRooms.add(room2);
-        System.out.println(ListOfRooms);
-        return ok(views.html.rooms.render((ArrayList<Rooms>) ListOfRooms));
+        room3.setFloor(3);
+        room1.setIdPhoto(1);
+        room2.setIdPhoto(2);
+        room3.setIdPhoto(3);
+        room1.setNumberOfBeds(2);
+        room2.setNumberOfBeds(2);
+        room3.setNumberOfBeds(3);
+        ListOfRoom.add(room1);
+        ListOfRoom.add(room2);
+        ListOfRoom.add(room3);
+        System.out.println(ListOfRoom);
+
+        List<Photo> ListOfPhoto = Model.<Photo>findAll(Photo.class);
+        Photo photo1 = new Photo();
+        Photo photo2 = new Photo();
+        Photo photo3 = new Photo();
+        photo1.setId(1);
+        photo2.setId(2);
+        photo3.setId(3);
+        photo1.setFilepath("assets/images/room.jpg");
+        photo2.setFilepath("assets/images/room1.jpg");
+        photo3.setFilepath("assets/images/room2.jpg");
+        ListOfPhoto.add(photo1);
+        ListOfPhoto.add(photo2);
+        ListOfPhoto.add(photo3);
+        return ok(views.html.rooms.render(ListOfRoom, ListOfPhoto));
     }
 }
